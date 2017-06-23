228b14d60a929007e74a78ef04b1e097a7224b0d
Steve Corsi <srcorsi@usgs.gov>
2017-06-23 16:40:53 -0500

    Working through adding summary variables to the MMSD P4 data set. Finding an error when computing sags on line 93.

--- scripts/1_process/getSummaryOpticalMMSDP4Mar032016.R
+++ scripts/1_process/getSummaryOpticalMMSDP4Mar032016.R
@@ -1,20 +1,14 @@
 # Script with workflow for adding summary optical data from vectorized
-# fluorescence and full absorbance scans
-
-###################
-#Set working directory
-SNsys <- system("wmic bios get serialnumber", intern = TRUE, show.output.on.console = FALSE)
-SN <- gsub("\\s","",SNsys)[2]
-baseDir <- "MMSD/Phase IV/virus/Data/Optical"
-if(SN == "PB519R1") {Project <- paste("D:/srcldata/",baseDir,sep="")
-}else {Project <- paste("M:/QW Monitoring Team/",baseDir,sep="")
-}
-
-setwd(Project)
-###################
+# fluorescence and full absorbance scans for MMSD Phase IV virus (Menomonee, Milwaukee, Bark Rivers)
 
 library(USGSHydroOpt)
 library(data.table)
+
+#set data directories
+raw.path <- "raw_data"
+cached.path <- "cached_data"
+summary.path <- "SummaryVariables"
+cached.save <- "0_munge"
 
 checkDups <- function(df,parm){
   df[duplicated(df[,parm]),parm]
@@ -23,29 +17,49 @@ checkDups <- function(df,parm){
 # Load fluorescence and absorbnce data
 
 ##########################################################################
-# dffl <- fread('MMSDFlmx4.csv',sep = ',',stringsAsFactors = FALSE)
+# dffl <- fread(file.path(raw.path,"PhaseIV","MMSDFlmx4v2.csv"),stringsAsFactors = FALSE)
 # dfabs <- fread('MMSDAbs.csv')
 # ## Generate 3d array of EEMs data ##
 # MMSDP43DEEMs <-VectorizedTo3DArray(df = dffl,ExEm="ex/em",grnum='GRnumber')
 # save(dffl,dfabs,MMSDP43DEEMs,file='MMSDOpticalData.RData')
 # 
-load('MMSDOpticalData.RData')
+# load('MMSDOpticalData.RData')
 # dfOpt <- read.csv('MMSDOptSummary.csv',stringsAsFactors = FALSE,skip=1)
 # 
 # dfOpt1 <- dfOpt[,-(which(names(dfOpt)=="A254"):dim(dfOpt)[2])]
-load(file='../merged/VirusPhaseIVData.Rdata')
-dfOpt1 <- df
 
-dfOpt1$GRnumber %in% substr(names(dffl),1,7)
 
-dfCheck <- dfOpt1[dfOpt1$GRnumber %in% names(dffl),]
+# Load summary data, 3-D fluorescence, and absorbance data
 
+
+load(file.path(raw.path,"PhaseIV","VirusPhaseIVData.Rdata"))
+load(file.path(raw.path,"PhaseIV","MMSDOpticalData.RData"))
+load(file.path(raw.path,summary.path,"ratioOrder2016-03-03.Rdata"))
+
+dfabs <- as.data.frame(dfabs) #convert from data.table to data.frame
+
+#Generate 3-D EEMS array
+MMSDP43DEEMs <-VectorizedTo3DArray(df = dffl,ExEm="ex/em",grnum='GRnumber')
+
+#Name the wavelength column in the abs file
+names(dfabs)[1] <- "Wavelength"
+
+dfOpt1 <- df[,-(which(names(df)=="A254"):dim(df)[2])]
 #dfOpt1$GRnumber %in% names(dfabs)
-load("M:/QW Monitoring Team/GLRI toxics/Shared_optical_data/GLRI/Final data/ratioOrder2016-03-03.Rdata") # Ratio orders determined by GLRI data set
+
+which(!(dfOpt1$GRnumber %in% names(dfabs)))
+dfOpt1[which(!(dfOpt1$GRnumber %in% substr(names(dffl),1,7))),"GRnumber"]
+
+#Missing samples in here are included in the Aqualog analysis but not the fluoromax
+dfMissingFl <- dfOpt1[which(!(dfOpt1$GRnumber %in% substr(names(dffl),1,7))),]
+
+#Subset the summary file to include only those that are present in the fluorescence file
+dfOpt1 <- dfOpt1[which((dfOpt1$GRnumber %in% substr(names(dffl),1,7))),]
+
+names(dffl) <- substr(names(dffl),1,7)
 
 # Read summary signals to extract from Fl and abs info
-#SummaryDir <- "D:/SRCLData/AqualogProcessing/SummaryVariables/MMSD/"
-SummaryDir <- "M:/QW Monitoring Team/Optical sensors/AqualogProcessing/SummaryVariables/MMSD/"
+SummaryDir <- paste0("./",raw.path,"/",summary.path,"/")
 
 dfFlSignals <- read.csv(paste(SummaryDir,"ex_ems_means.csv",sep=""),as.is=TRUE)
 dfAbsSignals <- read.csv(paste(SummaryDir,"abs_wavs.csv",sep=""),as.is=TRUE)
@@ -69,11 +83,12 @@ logSignals <- read.csv(paste(SummaryDir,"logSignals.csv",sep=""),as.is=TRUE)[,1]
 dfOpt2 <- getMeanFl(a=MMSDP43DEEMs,signals=dfFlSignals,Peak="Peak",Ex1="Ex1",Ex2="Ex2",Em1="Em1",Em2="Em2",dataSummary=dfOpt1,grnum="GRnumber")
 
 #HIX, FI, Freshness 
-dfOpt2 <- getIndexes(a=MMSD3DEEMs,dataSummary=dfOpt2,grnum="GRnumber")
+dfOpt2 <- getIndexes(a=MMSDP43DEEMs,dataSummary=dfOpt2,grnum="GRnumber")
 
 #Single absorbance signals
 dfOpt2 <- getAbs(dataAbs=dfabs,waveCol="Wavelength",wavs=dfAbsSignals[,1],colSubsetString="gr",dataSummary=dfOpt2,grnum="GRnumber")
 
+#START HERE....PROBLEM WITH MISSING VALUE ON 
 #Spectral slopes
 dfOpt2 <- getSag(dataAbs=dfabs,waveCol="Wavelength",sag=dfSagSignals,colSubsetString="gr",dataSummary=dfOpt2,grnum="GRnumber")
 
@@ -105,15 +120,15 @@ for(var in names(dfOpt2)){
 names(bad) <- names(dfOpt2)
 bad[which(bad>0)]
 
-
-fDate <- as.character(Sys.Date())
-save(dfOptSumAll,file=paste("dfOptSummaryMMSDP3",fDate,".RData",sep=""))
-write.csv(dfOptSumAll,paste("dfOptSummaryMMSDP3",fDate,".csv",sep=""),row.names=FALSE)
-
-varNames <- names(dfOptSumAll)
-varNames
-write.csv(names(dfOptSumAll),file=paste("namesAllMMSDP3",fDate,".csv",sep=""),row.names=FALSE)
-
+# 
+# fDate <- as.character(Sys.Date())
+# save(dfOptSumAll,file=paste("dfOptSummaryMMSDP3",fDate,".RData",sep=""))
+# write.csv(dfOptSumAll,paste("dfOptSummaryMMSDP3",fDate,".csv",sep=""),row.names=FALSE)
+# 
+# varNames <- names(dfOptSumAll)
+# varNames
+# write.csv(names(dfOptSumAll),file=paste("namesAllMMSDP3",fDate,".csv",sep=""),row.names=FALSE)
+# 
 
 
 
