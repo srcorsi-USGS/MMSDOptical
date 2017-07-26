c6ebab8a58009371a014aeda562e36785d91a6a0
Steve Corsi <srcorsi@usgs.gov>
2017-07-26 15:56:13 -0500

    Addint continuous WQ data. Fixed bug in TSstormstats for periods of missing data.

---
+++ scripts/1_process/AddContinuousWQData2.R
@@ -0,0 +1,55 @@
+# Gather information from continuous water quality sensors for matching with
+# optical and bacteria data
+
+library(dplyr)
+library(USGSHydroTools)
+
+#set data directories
+munge.path <- "0_munge"
+WQ.path <- "ContinuousWQ"
+cached.path <- "cached_data"
+summary.path <- "SummaryVariables"
+summary.save <- "1_SummaryVariables"
+cached.save <- "0_munge"
+
+#Read dataframe with bacteria and optical data
+df <- readRDS(file.path(cached.path,summary.save,"dfOptP3P4Combined.rds"))
+df.orig <- df
+
+sites <- c("04087030","04087050","04087088","04087119","04087120","04087142","04086600","05426060")
+names(sites) <- c("MF","LD","UW","HW","MW","MC","CG","BK")
+# parms <- c("00060","00095","00300","00010","63680")
+# names(parms) <- c("Q","SC","DO","WT","Turbidity")
+parmNames <- c("Discharge","Dissolved_oxygen","Specific_cond","Turbidity","Water_Temperature")
+j <- 1
+i <- 2
+dfSum <- df
+
+#Add summary stats for each continuous water quality varible
+for(j in c(1,2,3,4,5)){
+  parm <- parmNames[j]
+  
+  #Read UV data file
+  dfWQ <- readRDS(file.path(cached.path,munge.path, WQ.path,filenm))
+  dfList <- list()
+  for(i in 1:6){
+    site <- names(sites[i])
+    filenm <- paste0(parm,"AllSites.rds")
+    subdf <- subset(dfWQ,STAID == sites[i])
+    subdfSum <- subset(dfSum,abbrev==names(sites[i]))
+
+    #Add columns that represent the stats for each individual WQ parm
+    dfList[[i]] <- TSstormstats(df = dfWQ,date="pdate",varname="Value",dates=subdfSum,
+                                starttime="psdate",endtime="pedate",
+                                stats.return = c("mean","median","max","min"),
+                                out.varname = parm)
+  }
+  
+  #Combine individual sites back into one file
+  dfSum <- dfList[[1]]
+  for(k in 2:length(dfList)){
+    dfSum <- rbind(dfSum,dfList[[k]])
+  }
+}
+
+saveRDS(dfSum,file.path(cached.path,summary.save,"dfOptP3P4CombinedContWQ.rds"))
---
+++ scripts/1_process/TSstormstats.R
@@ -0,0 +1,69 @@
+df <- dfWQ;date<-"pdate";varname<-"Value";dates<-df.orig;
+starttime<-"psdate";endtime<-"pedate";
+stats.return <- c("mean","median","max","min");
+subdfvar<-"STAID";subdfvalue<-sites[i];
+subdatesvar<-"abbrev";subdatesvalue<-site;
+out.varname <- parm
+
+
+
+TSstormstats <- function (df, date = "pdate", varname, dates, starttime = "Ebpdate", 
+          endtime = "Eepdate", stats.return = c("mean"), subdfvar = "", 
+          subdfvalue = "", subdatesvar = "", subdatesvalue = "", out.varname = "") 
+{
+  stats.names <- c("mean", "max", "min", "median", "sum", "sd", 
+                   "maxdiff", "difference", "nearest", "nearprev")
+  stats.get <- data.frame(row.names = 1)
+  stats.get[, stats.names[1:length(stats.names)]] <- FALSE
+  nstats <- length(stats.return)
+  stats.get[, stats.return[1:nstats]] <- TRUE
+  stats.return <- names(stats.get[which(stats.get[1, ] == TRUE)])
+  if (subdfvar != "") 
+    df <- subset(df, df[, subdfvar] == subdfvalue)
+  if (subdatesvar != "") 
+    dates <- subset(dates, dates[, subdatesvar] == subdatesvalue)
+  maxrows <- nrow(dates)
+  varstats = data.frame(row.names = 1:maxrows)
+  resultname = vector(mode = "character")
+  varcols <- which(names(df) == varname)
+  for (k in 1:length(varcols)) {
+    varname <- names(df)[varcols[k]]
+    for (i in 1:maxrows) {
+      subdata <- df[which(df[, date] >= dates[i, starttime] & 
+                            df[, date] <= dates[i, endtime]), ]
+      if(sum(!is.na(subdata[,varname]))>0){
+      if (stats.get[, "mean"]) 
+        varstats[i, "mean"] <- mean(subdata[, varname], 
+                                    na.rm = T)
+      if (stats.get[, "max"]) 
+        varstats[i, "max"] <- max(subdata[, varname], 
+                                  na.rm = T)
+      if (stats.get[, "min"]) 
+        varstats[i, "min"] <- min(subdata[, varname], 
+                                  na.rm = T)
+      if (stats.get[, "median"]) 
+        varstats[i, "median"] <- median(subdata[, varname], 
+                                        na.rm = T)
+      if (stats.get[, "sum"]) 
+        varstats[i, "sum"] <- sum(subdata[, varname], 
+                                  na.rm = T)
+      if (stats.get[, "sd"]) 
+        varstats[i, "sd"] <- sd(subdata[, varname], na.rm = T)
+      if (stats.get[, "maxdiff"]) 
+        varstats[i, "maxdiff"] <- max(subdata[, varname], 
+                                      na.rm = T) - min(subdata[, varname], na.rm = T)
+      if (stats.get[, "difference"]) 
+        varstats[i, "difference"] <- subdata[nrow(subdata), 
+                                             varname] - subdata[1, varname]
+      }
+        
+      statsnames <- stats.return
+    }
+    rm(resultname)
+    resultname <- paste(out.varname, "_", stats.return, sep = "")
+    names(varstats) <- resultname
+    dates <- cbind(dates, varstats)
+    varstats <- varstats[, -(1:length(varstats))]
+  }
+  return(dates)
+}
